039302d214dd7173854d8620a08c5d97025f4528
StrangerTwo <h.volhejn@gmail.com>
2021-12-14 12:56:09 +0100

    added paralel download

--- WholeNetDownloader/Program.cs
+++ WholeNetDownloader/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 
@@ -12,13 +14,13 @@ namespace WholeNetDownloader
 {
     class Program
     {
-        async static Task Main(string[] args)
+        static void Main(string[] args)
         {
             var WWWDownloader = new NetDownloader("https://www.delta-skola.cz/", "trash");
 
             Process.Start("trash");
 
-            await WWWDownloader.Download();
+            WWWDownloader.Download();
 
             Console.WriteLine("Work Finished");
             Console.ReadKey();
@@ -31,6 +33,8 @@ namespace WholeNetDownloader
         private long _lastNotifySize = 0;
         private DirectoryInfo directory;
         private Uri _startUri;
+        private ConcurrentQueue<Uri> downloadQueue;
+        private int runningDownloadsCount = 0;
 
         public NetDownloader(string startUrl, string directoryPath)
         {
@@ -38,7 +42,18 @@ namespace WholeNetDownloader
             directory = new DirectoryInfo(directoryPath);
         }
 
-        async public Task Download()
+        private void NotifyUser()
+        {
+            if (_lastNotifySize + NotifyMemoryVar < DirSize(directory))
+            {
+                _lastNotifySize = DirSize(directory);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Stáhlo se již {0}KB", _lastNotifySize / 1024);
+                Console.ResetColor();
+            }
+        }
+
+        public void Download()
         {
             if (directory.Exists)
             {
@@ -51,19 +66,30 @@ namespace WholeNetDownloader
             {
                 Directory.CreateDirectory(directory.FullName);
             }
-            await DownloadPage(_startUri);
+
+            downloadQueue = new ConcurrentQueue<Uri>();
+            downloadQueue.Enqueue(_startUri);
+
+            StartNewDownload();
+        }
+
+        private void StartNewDownload()
+        {
+            while (downloadQueue.Count > 0 || runningDownloadsCount > 0)
+            {
+                if (downloadQueue.TryDequeue(out Uri link))
+                {
+                    _ = DownloadPage(link);
+                }
+            }
         }
 
         async private Task DownloadPage(Uri uri)
         {
+            NotifyUser();
             if (directory.GetFiles().Select(x => x.Name).Contains(GetFileNameFromUri(uri)))
             {
                 return;
-            }
-            if (_lastNotifySize + NotifyMemoryVar < DirSize(directory))
-            {
-                _lastNotifySize = DirSize(directory);
-                Console.WriteLine("Stáhlo se již {0}KB", _lastNotifySize / 1024);
             }
 
             using (var webClient = new WebClient
@@ -72,47 +98,50 @@ namespace WholeNetDownloader
             }
             )
             {
-                Console.Write("Pokouším se o stažení {0}", uri.ToString());
+                Console.WriteLine("{0}", uri.ToString());
                 string downloadedStr;
+                Interlocked.Increment(ref runningDownloadsCount);
                 try
                 {
                     downloadedStr = await webClient.DownloadStringTaskAsync(uri);
                     File.WriteAllText(Path.Combine(directory.FullName, GetFileNameFromUri(uri)), downloadedStr, Encoding.UTF8);
-                    Console.WriteLine(" OK");
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
-                    Console.WriteLine(" FAIL");
                     return;
                 }
 
                 try
                 {
-                    // Parsnout, najít další odkazy, rekurze
                     var doc = new HtmlDocument();
                     doc.LoadHtml(downloadedStr);
 
                     var body = doc.DocumentNode.SelectSingleNode("html/body");
-                    if (body != null)
+                    if (body == null) return;
+
+                    var addressNodes = body.SelectNodes(".//a");
+                    if (addressNodes == null) return;
+
+
+                    List<Uri> pageLinks = new List<Uri>();
+                    foreach (var address in addressNodes)
                     {
-                        var nodes = body.SelectNodes(".//a");
-                        if (nodes == null)
+                        if (address.Attributes.Contains("href") && address.Attributes["href"].Value.StartsWith("http"))
                         {
-                            return;
-                        }
-                        foreach (var link in nodes)
-                        {
-                            if (link.Attributes.Contains("href") && link.Attributes["href"].Value.StartsWith("http"))
+                            string addressHref = address.Attributes["href"].Value;
+                            if (addressHref.Contains("?"))
                             {
-                                string address = link.Attributes["href"].Value;
-                                if (address.Contains("?"))
-                                {
-                                    address = address.Remove(address.IndexOf("?"));
-                                }
-                                await DownloadPage(new Uri(address));
+                                addressHref = addressHref.Remove(addressHref.IndexOf("?"));
                             }
+                            pageLinks.Add(new Uri(addressHref));
                         }
                     }
+
+                    foreach (var link in pageLinks)
+                    {
+                        downloadQueue.Enqueue(link);
+                    }
+                    Interlocked.Decrement(ref runningDownloadsCount);
                 }
                 catch (Exception e)
                 {
